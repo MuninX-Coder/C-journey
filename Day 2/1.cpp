bcc15b09c3db535bcc8b3fb2a0264413ed0104be
MuninX-Coder <muninborah1825@gmail.com>
2025-08-01 19:59:37 +0530

    Add files via upload

---
+++ Day 2/1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 5;
+    cout << x << endl;
+    x += 2;
+    cout << x << endl;
+    x -= 8;
+    cout << x << endl;
+    x *= 10;
+    cout << x << endl;
+
+    // arithmetic operators 
+
+    int y = 2, z = 5;
+    cout << y + z << endl;  // (+) addition
+    cout << y - z << endl;  // (-) subtraction
+    cout << y * z << endl;  // (*)  multiplication
+    cout << y / z << endl;  // (/)  division
+    return 0;
+}
